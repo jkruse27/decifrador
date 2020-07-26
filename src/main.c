3ca7db0bbe95da97de5524e0718fd6f902bd9cc9
João <soyjog@gmail.com>
2020-07-25 22:18:10 -0300

    Criei função de conversão inversa | Passou em todos os testes

--- src/main.c
+++ src/main.c
@@ -6,18 +6,38 @@
 
 #include <stdio.h>
 
-#define TAM_BUFFER 50
+#define TAM_BUFFER 60
 char buffer_entrada[TAM_BUFFER];
 char buffer_saida[TAM_BUFFER];
 
+const char senha[] = "SENHASECRETA";
+
+char contrasenha(char a, char b){
+	if(a < 'A')
+		return a;
+	
+	a = a - 'A';
+	char i;
+	char k, c;	
+	for(i = 'A', k = 'Z'-'A'+1; (i+b+2*'A')%k != a; i++);
+	
+	return i; 
+}
+
 int main() {
-  /* Leitura da entrada */
-  fgets(buffer_entrada, TAM_BUFFER, stdin);
+	/* Leitura da entrada */
+	fgets(buffer_entrada, TAM_BUFFER, stdin);
 
+	int i, j;
+	for (i=0; (i<TAM_BUFFER) && (buffer_entrada[i]!='\0') && (buffer_entrada[i]!='\n'); i++) {
+		buffer_saida[i] = contrasenha(buffer_entrada[i], senha[j]);
+		j++;
+		if (senha[j]=='\0') j=0;
+	}
+	buffer_saida[i]='\0';
 
+	/* Escrever saida na tela */
+	printf("%s\n", buffer_saida);
 
-  /* Escrever saida na tela */
-  printf("%s\n", buffer_saida);
-
-  return 0;
+	return 0;
 }
